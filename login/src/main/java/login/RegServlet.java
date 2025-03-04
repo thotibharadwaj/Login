26fd366ed8a9b7fa05c3eba3ac036879977d8ae8
thotibharadwaj <thotibharadwaj@gmail.com>
2025-03-04 08:30:58 +0530

    First Commit

---
+++ login/src/main/java/login/RegServlet.java
@@ -0,0 +1,92 @@
+package login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegServlet
+ */
+@WebServlet("/reg")
+public class RegServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     Connection con;  
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		    con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl1","c##bunny","password");
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		try {
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		try {
+			String s1 = request.getParameter("fname");
+			String s2 = request.getParameter("lname");
+			String s3 = request.getParameter("uname");
+			String s4 = request.getParameter("pword");
+			PreparedStatement pstmt = con.prepareStatement("insert into uinfo values(?,?,?,?)");
+			pstmt.setString(1, s1);
+			pstmt.setString(2, s2);
+			pstmt.setString(3, s3);
+			pstmt.setString(4, s4);
+			pstmt.executeUpdate();
+			
+			PrintWriter pw = response.getWriter();
+			pw.println("<html><body bgcolor=pink text=red><center>");
+			pw.println("<h1>You Have Registered Successfully</h1>");
+			pw.println("<a href=login.html>Login</a>");
+			pw.println("</center></body></html>");
+		} catch (SQLException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}	
+
+
+}
